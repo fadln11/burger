204409e53c19a2b68ac813002e8e74cb79806ccc
fadln11 <fadeline314@gmail.com>
2019-03-23 16:21:36 -0500

    add jawsdb conn for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,29 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "monty",
-  password: "password",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "monty",
+//   password: "password",
+//   database: "burgers_db"
+// });
+
+
+// Heroku JawsDB or local
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Heroku JawsDB Database
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Local Database
+    connection = mysql.createConnection({
+      port: 3306,
+      host: "localhost",
+      user: "monty",
+      password: "password",
+      database: "burgers_db"
+    });
+};
 
 connection.connect(function(err) {
   if (err) {
